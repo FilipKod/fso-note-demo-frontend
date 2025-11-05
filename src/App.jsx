cbafdeecb20451dfe9bfc2f00c22fabdd601cbe6
Filip Madunicky <yelidric@gmail.com>
2025-11-05 11:28:49 +0100

    Adds user login functionality
    Adds functionality for user login, including storing user data in local storage and setting a token for authorized requests. Implements a login form and conditionally renders notes based on login status. Also creates a new service for handling login requests.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,31 @@ import Note from "./components/Note";
 import noteService from "./services/notes";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import loginService from './services/login'
 
 const App = () => {
   const [appNotes, setAppNotes] = useState(null);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialState) => {
       setAppNotes(initialState);
     });
   }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault();
@@ -57,10 +70,73 @@ const App = () => {
       });
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+   
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedNoteAppUser', JSON.stringify(user)
+      )
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input 
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input 
+              type="text"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+  )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged</p>
+          {noteForm()}
+        </div>
+      )}      
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -76,10 +152,7 @@ const App = () => {
             />
           ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      
       <Footer />
     </div>
   );
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 const baseUrl = "/api/notes";
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   const nonExisting = {
@@ -12,7 +18,10 @@ const getAll = () => {
 };
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request = axios.post(baseUrl, newObject, config);
   return request.then((response) => response.data);
 };
 
@@ -25,4 +34,5 @@ export default {
   getAll,
   create,
   update,
+  setToken
 };
